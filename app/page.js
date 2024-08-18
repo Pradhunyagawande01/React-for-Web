31ed6ffa00012eef5dceb95f328fa8896b8ebc53
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2024-08-18 22:47:13 +0530

    secon commit

--- app/page.js
+++ app/page.js
@@ -1,25 +1,43 @@
 
-import { Input } from 'postcss'
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("") 
+
+  const submitHandler = (e) =>{
+e.preventDefault()
+console.log(title)
+console.log(desc)
+settitle("")
+setdesc("")
+  }
   return (
     <>
-    
-    <h1 className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-center px-3 text-black text-2xl font-bold h-25 py-2 w-full gradient-red ">
-      Welcome To My Todo Application
+      <h1 className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-center px-3 text-black text-2xl font-bold h-25 py-2 w-full gradient-red ">
+          Welcome To My Todo Application
       </h1>
-      <form>
+      <form onSubmit={submitHandler}>
           <input
           type="text"
           className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2"
-          placeholder="Enter Your Task"
+          placeholder="Enter Your Title"
+          value={title}
+          onChange={(e)=>{
+            settitle(e.target.value);
+          }}
           />
 
            <input
           type="text"
           className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2"
           placeholder="Enter Discription"
+          value={desc}
+          onChange={(e)=>{
+            setdesc(e.target.value);
+          }}
           />
           <button className="bg-black text-white m-5 px-4 py-3 text-2xl font-bold rounded ">Add Task</button>
       </form>
